2eac84ca149f4879ffd9faba02db0f6fb3b05e50
Ferit Yiğit BALABAN <fyb@fybx.dev>
2024-03-03 15:49:43 +0300

    Refactor gulpfile.js for improved readability and organization
    Signed-off-by: Ferit Yiğit BALABAN <fyb@fybx.dev>

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,17 @@ var gulp = require('gulp'),
 	browserSync = require('browser-sync'),
 	autoprefixer = require('gulp-autoprefixer'),
 	sass = require('gulp-sass')(require('sass')),
-	minify = require('gulp-minify')
+	minify = require('gulp-minify');
 
 gulp.task('sass', function () {
-	return gulp.src('src/sass/**/*.sass')
+	return gulp
+		.src('src/sass/**/*.sass')
 		.pipe(sass())
-		.pipe(autoprefixer([
-			'last 10 versions'
-		], {
-			cascade: true
-		}))
+		.pipe(
+			autoprefixer(['last 10 versions'], {
+				cascade: true,
+			})
+		)
 		.pipe(gulp.dest('src/css/'))
 		.pipe(browserSync.reload({ stream: true }));
 });
@@ -19,32 +20,33 @@ gulp.task('sass', function () {
 gulp.task('browser-sync', function () {
 	browserSync({
 		server: {
-			baseDir: 'src'
+			baseDir: 'src',
 		},
-		notify: false
+		notify: false,
 	});
 });
 
 gulp.task('img', function () {
-	return gulp.src('src/img/**/*')
-		.pipe(gulp.dest('dist/img'));
+	return gulp.src('src/img/**/*').pipe(gulp.dest('dist/img'));
 });
 
 gulp.task('video', function () {
-	return gulp.src('src/video/**/*')
-		.pipe(gulp.dest('dist/video'));
+	return gulp.src('src/video/**/*').pipe(gulp.dest('dist/video'));
 });
 
 gulp.task('min-js', function () {
-	return gulp.src('src/js/*.js')
-		.pipe(minify({
-			noSource: true,
-			ext: {
-				min: '.min.js'
-			},
-			ignoreFiles: ['min.js']
-		}))
-		.pipe(gulp.dest('src/js/min'))
+	return gulp
+		.src('src/js/*.js')
+		.pipe(
+			minify({
+				noSource: true,
+				ext: {
+					min: '.min.js',
+				},
+				ignoreFiles: ['min.js'],
+			})
+		)
+		.pipe(gulp.dest('src/js/min'));
 });
 
 gulp.task('watch', function () {
@@ -55,30 +57,15 @@ gulp.task('watch', function () {
 });
 
 gulp.task('prebuild', async function () {
+	var buildCss = gulp.src('src/css/**/*').pipe(gulp.dest('dist/css'));
 
-	var buildCss = gulp.src('src/css/**/*')
-		.pipe(gulp.dest('dist/css'));
+	var buildFonts = gulp.src('src/fonts/**/*').pipe(gulp.dest('dist/fonts'));
 
-	var buildFonts = gulp.src('src/fonts/**/*')
-		.pipe(gulp.dest('dist/fonts'));
+	var buildJs = gulp.src('src/js/**/*').pipe(gulp.dest('dist/js'));
 
-	var buildJs = gulp.src('src/js/**/*')
-		.pipe(gulp.dest('dist/js'));
-
-	var buildHtml = gulp.src('src/*.html')
-		.pipe(gulp.dest('dist'));
-
+	var buildHtml = gulp.src('src/*.html').pipe(gulp.dest('dist'));
 });
 
 gulp.task('build', gulp.parallel('prebuild', 'img', 'video', 'sass', 'min-js'));
 
 gulp.task('default', gulp.parallel('watch', 'browser-sync', 'sass', 'min-js'));
-
-
-
-
-
-
-
-
-
