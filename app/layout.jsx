5afd7083d40ddcfa80720cad2a562333a8e77bb6
osmannyildiz <iamosmannyildiz@gmail.com>
2024-05-26 18:54:20 +0300

    add bootstrap and sass, add some files from old version

---
+++ app/layout.jsx
@@ -0,0 +1,19 @@
+import "./globals.sass";
+
+export const metadata = {
+	title: "Atlaspad",
+	description:
+		"Atlaspad, introduces a the first and only trustless Cross-Chain ZK Launchpad that utilizes zero-knowledge proofs to enable secure and private transactions across multiple blockchains.",
+};
+
+export default function RootLayout({ children }) {
+	return (
+		<html lang="en">
+			<head>
+				<link rel="icon" type="image/png" href="/img/atlasLogo.svg" />
+				<link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+			</head>
+			<body>{children}</body>
+		</html>
+	);
+}
---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+export default function Home() {
+	return (
+		<div>
+			<h1>Atlaspad</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas recusandae
+				nihil molestiae sapiente unde. Soluta expedita iste officiis corporis
+				ipsam neque nulla sit sapiente minima, corrupti necessitatibus autem.
+				Commodi, quisquam.
+			</p>
+		</div>
+	);
+}
